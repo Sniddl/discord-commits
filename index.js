d3937a7788a9af293d3bf06b2de694f559df274b
Zeb <burroughszeb@me.com>
2020-04-10 16:48:04 -0500

    f

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const _ = require("lodash");
 
-try {
-  const message = core.getInput("message");
-  const webhook = core.getInput("webhook");
-  const commit = core.getInput("commit");
+_.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
 
-  console.log(message);
-  console.log(commit);
-  console.log(github);
-} catch (error) {
-  core.setFailed(error.message);
-}
+// try {
+//   const message = core.getInput("message");
+//   const webhook = core.getInput("webhook");
+//   const commit = core.getInput("commit");
+
+//   console.log(message);
+//   console.log(commit);
+//   console.log(github);
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
+
+const data = { env: { ...process.env } };
+const content = _.template("hello, {{ data }}")({ data: "asdf" });
+console.log(data);
+console.log(github);
