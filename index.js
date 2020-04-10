82521712a5d7e67c59406664b2ec6950551d81b1
Zeb <burroughszeb@me.com>
2020-04-10 17:06:37 -0500

    g

--- index.js
+++ index.js
@@ -4,19 +4,35 @@ const _ = require("lodash");
 
 _.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
 
-// try {
-//   const message = core.getInput("message");
-//   const webhook = core.getInput("webhook");
-//   const commit = core.getInput("commit");
+try {
+  const message = core.getInput("message");
+  const webhook = core.getInput("webhook");
+  const embed = core.getInput("embed") || '{ "title": "{{ env.OS }}" }';
 
-//   console.log(message);
-//   console.log(commit);
-//   console.log(github);
-// } catch (error) {
-//   core.setFailed(error.message);
-// }
+  github.context.payload.commits = github.context.payload.commits || [
+    { message: "this is a title\n\nthis is a description" },
+    { message: "this is a title\n\nthis is a description" },
+  ];
+  console.log(github.context.payload.commits);
 
-const data = { env: { ...process.env } };
-const content = _.template("hello, {{ data }}")({ data: "asdf" });
-console.log(data);
-console.log(github);
+  const embeds = github.context.payload.commits.map((commit) => {
+    const $data = {
+      env: { ...process.env },
+      github: { ...github },
+      commit: {
+        title: commit.message.split("\n\n").slice(0, 1).join("\n\n"),
+        description: commit.message.split("\n\n").slice(1).join("\n\n"),
+        ...commit,
+      },
+    };
+
+    console.log($data);
+
+    return JSON.parse(_.template(embed)($data));
+  });
+
+  console.dir(github);
+  console.dir(embeds);
+} catch (error) {
+  core.setFailed(error.message);
+}
